c865871624101e6bcc584bee43ef6f6a9b13520a
halahabaybeh <hala.habaibe@gmail.com>
2026-08-12 21:06:16 +0300

    add swiper event listener to categories

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const headertag = document.querySelector(".header-bg");
 const navLinks = document.querySelectorAll(".nav-link");
 const logo = document.querySelector(".logo a");
 const scrollSearch = document.querySelectorAll(".scrollSearch");
-
+const bookForm = document.querySelector(".bookForm");
+console.log(bookForm);
 
 window.addEventListener("scroll", () =>{
 
@@ -29,4 +30,36 @@ window.addEventListener("scroll", () =>{
         headertag.classList.add("border-b");
     }
 });
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 20, // المسافة بين الكروت
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  
+  },
+
+  // التحكم بظهور عدد السلايدات حسب الشاشة
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5, 
+      spaceBetween: 10, // سلايد وجزء من الثاني للموبايل
+    },
+    640: {
+      slidesPerView: 2.2,
+      spaceBetween: 15, // سلايدين وجزء
+    },
+    1024: {
+      slidesPerView: 3.5, // 3 سلايدات ونص للشاشات الكبيرة
+    },
+  },
+});
+
 
